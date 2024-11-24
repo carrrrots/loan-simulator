eeb748a378c364293a1322191ee13b6ca50d8ea9
Caroline <caroline6254@hotmail.com>
2024-11-23 23:27:21 -0300

    feat(modelo): criar modelo `LoanSimulation`

---
+++ app/models/loan_simulation.rb
@@ -0,0 +1,2 @@
+class LoanSimulation < ApplicationRecord
+end
---
+++ db/migrate/20241123221956_create_loan_simulations.rb
@@ -0,0 +1,15 @@
+class CreateLoanSimulations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :loan_simulations do |t|
+      t.decimal :loan_amount
+      t.date :birthdate
+      t.integer :loan_term
+      t.decimal :interest_rate
+      t.decimal :monthly_payment
+      t.decimal :total_amount
+      t.decimal :total_interest
+
+      t.timestamps
+    end
+  end
+end
